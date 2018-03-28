d7e91514db75ea346495a6a932bae8503f544d3f
unknown <marina.iabluchanskaya@gmail.com>
2018-03-28 15:46:55 +0200

    design update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,116 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 import "./index.css";
+
+const Wrapper = styled.section`
+  color: #8a8a8a;
+  font-family: "Lato", sans-serif;
+`;
+const WrapperSearch = styled.section`
+  padding: 14px;
+  border-bottom: 1px solid #e7e7e7;
+  border-top: 1px solid #e7e7e7;
+  background-color: #fff;
+  padding: 30px 4% 20px;
+  margin: 2% 0;
+  form {
+    margin-bottom: 10px;
+  }
+`;
+const Header = styled.h1`
+  color: #16253c;
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+const Button = styled.button`
+  background:#DB7093 ;
+  color:white ;
+  font-size: 16px;
+  margin: 16px;
+  padding: 4px 16px;
+  border: 2px solid #DB7093;
+  border-radius: 6px;
+  outline: none;
+  font-family: "Lato", sans-serif;
+  font-weight:bold;
+  transition: all 0.3s ease 0s;
+ 
+  &:hover {
+    background:white ;
+      color: #DB7093 ;  
+}
+&:active { transform: translateY(2px); }
+  }
+`;
+const ButtonClose = styled.button`
+  cursor: pointer;
+  color: #fff;
+  border-radius: 30px;
+  background: #db7093;
+  font-size: 31px;
+  font-weight: bold;
+  position: absolute;
+  line-height: 0px;
+  padding: 13px 5px;
+  top: -10px;
+  right: -10px;
+  border: none;
+  outline: none;
+  font-weight: normal;
+  transition: all 0.3s ease 0s;
+  &:before {
+    content: "×";
+  }
+  &:hover {
+    padding: 15px 7px;
+    top: -12px;
+    right: -12px;
+  }
+  &:active { transform: translateY(2px); }
+  }
+`;
+const Input = styled.input`
+  color: #555;
+  padding: 6px;
+  margin: 6px;
+  background: white;
+  border: 2px solid #db7093;
+  border-radius: 6px;
+  outline: none;
+  font-family: "Lato", sans-serif;
+  width: 12%;
+  min-width: 225px;
+`;
+const SearchElements = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: stretch;
+  align-content: stretch;
+
+  li {
+    opacity: 0.9;
+    width: 20%;
+    margin: 1%;
+    padding: 1%;
+    list-style: none;
+    background: white;
+    border-radius: 10px;
+    min-width: 150px;
+    position: relative;
+
+    p {
+      margin-bottom: 15px;
+    }
+    strong {
+      color: #263750;
+    }
+  }
+`;
 
 class SearchForm extends React.Component {
   constructor(props) {
@@ -49,8 +159,8 @@ class SearchForm extends React.Component {
   updateSearchResults(searchResult) {
     let isNewResult = true;
 
-    let updateValues = this.state.searchResults.forEach((item, i) => {
-      if (item.id == searchResult.id) {
+    this.state.searchResults.forEach((item, i) => {
+      if (item.id === searchResult.id) {
         isNewResult = false;
         this.state.searchResults.splice(i, 1, searchResult);
         this.setState({ dublicateMessage: "You have such search yet" });
@@ -98,13 +208,9 @@ class SearchForm extends React.Component {
   }
 
   render() {
-    localStorage.setItem(
-      "searchResults",
-      JSON.stringify(this.state.searchResults)
-    );
-    let getList = JSON.parse(localStorage.getItem("searchResults"));
+    let getList = this.Storage();
     let searchElements = getList.map(result => (
-      <li>
+      <li key={result.id}>
         <p>
           <strong>{result.value}</strong>
         </p>
@@ -112,26 +218,41 @@ class SearchForm extends React.Component {
         <p>Stars rating of repository: {result.starCount} </p>
         <p>Issues opened for repository: {result.issueCount} </p>
         <p>Rating of repository: {result.rating} </p>
-        <button onClick={() => this.handleRemove(result.id)}>Delete</button>
+        <ButtonClose onClick={() => this.handleRemove(result.id)} />
       </li>
     ));
     return (
-      <div>
-        <form onSubmit={this.handleClickSubmit}>
-          <label>
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Search" />
-        </form>
-        <p>{this.state.errorMessage} </p>
-        <p>{this.state.dublicateMessage} </p>
-        <ul className="search-list">{searchElements}</ul>
-      </div>
+      <Wrapper>
+        <WrapperSearch>
+          <Header>Search for repository</Header>
+          <form onSubmit={this.handleClickSubmit}>
+            <label>
+              <Input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
+            </label>
+            <Button type="submit" value="Search">
+              Search
+            </Button>
+          </form>
+          <p>{this.state.errorMessage} </p>
+          <p>{this.state.dublicateMessage} </p>
+        </WrapperSearch>
+
+        <SearchElements>{searchElements}</SearchElements>
+      </Wrapper>
     );
+  }
+
+  Storage() {
+    localStorage.setItem(
+      "searchResults",
+      JSON.stringify(this.state.searchResults)
+    );
+    let getList = JSON.parse(localStorage.getItem("searchResults"));
+    return getList;
   }
 }
 
